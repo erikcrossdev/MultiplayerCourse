9f60b1d3966781ee81c5d6b931fa05391fb3e2e8
ErikCruz <erikcruzdev@gmail.com>
2025-05-26 21:01:26 -0300

    Add test join and host game; create box with replication movement

--- Source/MultiplayerCourse/MultiplayerCourseGameMode.cpp
+++ Source/MultiplayerCourse/MultiplayerCourseGameMode.cpp
@@ -13,3 +13,17 @@ AMultiplayerCourseGameMode::AMultiplayerCourseGameMode()
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 }
+
+void AMultiplayerCourseGameMode::HostLANGame()
+{
+	GetWorld()->ServerTravel("/Game/ThirdPerson/Maps/ThirdPersonMap?listen"); //our map path and load it
+	//?listen means that it will start as a ListenServer
+}
+
+void AMultiplayerCourseGameMode::JoinLANGame()
+{
+	APlayerController* PC = GetGameInstance()->GetFirstLocalPlayerController();
+	if (PC) {
+		PC->ClientTravel("10.0.0.18",TRAVEL_Absolute); //getting our local ipv4
+	}
+}
